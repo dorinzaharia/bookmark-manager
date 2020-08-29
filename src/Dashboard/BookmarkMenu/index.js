d94bdecc0c147fae6610b82b36dd074299a1c258
dorinzaharia <dorin.zahariazd@gmail.com>
2020-08-29 20:39:37 +0300

    Add BookmarkMenu to the Dashboard
    Add horizontal menu for routing.

---
+++ src/Dashboard/BookmarkMenu/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Menu } from "antd";
+import {
+  InboxOutlined,
+  FolderOpenOutlined,
+  TagsOutlined,
+  AppstoreOutlined,
+} from "@ant-design/icons";
+
+const { SubMenu } = Menu;
+const BookmarkMenu = () => {
+  return (
+    <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+      <Menu.Item key="1" icon={<AppstoreOutlined />}>
+        All
+      </Menu.Item>
+      <Menu.Item key="2" icon={<InboxOutlined />}>
+        Unsorted
+      </Menu.Item>
+      <SubMenu
+        key="sub1"
+        icon={<FolderOpenOutlined />}
+        title="Collections"
+      ></SubMenu>
+      <Menu.Item key="3" icon={<TagsOutlined />}>
+        Tags
+      </Menu.Item>
+    </Menu>
+  );
+};
+
+export default BookmarkMenu;
--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,38 +1,74 @@
 // External imports
 import React, { Component } from "react";
-import { Layout, Menu, Breadcrumb } from "antd";
-import Title from "antd/lib/typography/Title";
+import { Layout, Menu, Button } from "antd";
 import Avatar from "antd/lib/avatar/avatar";
-	// External imports: CSS
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  SettingOutlined,
+  BookOutlined,
+  SearchOutlined,
+  PlusSquareOutlined,
+} from "@ant-design/icons";
+  // CSS
 import "antd/dist/antd.css";
+import "./Dashboard.css";
+
+// Internal imports
+import BookmarkMenu from "./BookmarkMenu";
 
 const { Header, Sider, Content } = Layout;
 
 class Dashboard extends Component {
+  state = {
+    collapsed: false,
+    avatar: "DZ"
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
   render() {
     return (
-        <Layout style={{minHeight: "100vh"}}>
-          <Header style={{ padding: 15 }}>
-            <Avatar style={{ float: "right" }} />
-            <Title style={{ color: "white" }} level={3}>
-              App
-            </Title>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+            <Button type="primary" className="button-add">
+              <PlusSquareOutlined />
+            </Button>
+          <BookmarkMenu />
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
+              <Menu.Item key="1" icon={<BookOutlined />}></Menu.Item>
+              <Menu.Item key="2" icon={<SearchOutlined />}></Menu.Item>
+              <Menu.Item key="3" icon={<SettingOutlined />}></Menu.Item>
+            </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            {React.createElement(
+              this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+              {
+                className: "trigger",
+                onClick: this.toggle,
+              }
+            )}
+            <div className="avatar">
+            <Avatar className="avatar">{this.state.avatar}</Avatar>
+            </div>
           </Header>
-          <Layout>
-            <Sider >
-              <Menu defaultSelectedKeys={["Dashboard"]} mode="inline">
-                <Menu.Item key="Dashboard">Dashboard</Menu.Item>
-              </Menu>
-            </Sider>
-            <Layout>
-              <Content style={{ padding: "0 40px" }}>
-                <Breadcrumb style={{ margin: "16px 0" }}>
-                  <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
-                </Breadcrumb>
-              </Content>
-            </Layout>
-          </Layout>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+          </Content>
         </Layout>
+      </Layout>
     );
   }
 }
